4df9ec2cd913a0465b939857267312f016c7268d
arobaseSuulei <souleymanediallo2004@outlook.fr>
2024-12-20 22:15:36 +0100

    pokedex

---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+
+
+let btn=document.getElementById("btn");
+let image=document.getElementById("img")
+let nom=document.getElementById("name")
+let id=document.getElementById("id")
+
+const response=async()=>{
+
+    let randomNumber=Math.ceil(Math.random()*150)+1
+    let request = `https://pokeapi.co/api/v2/pokemon/${randomNumber}`
+
+    // fetching data
+    let data = await fetch(request)
+
+    // convertir la reponse en json
+    let response = await data.json()
+    console.log(response)
+
+
+    nom.textContent=response.name
+
+    id.textContent=response.id
+    image.src=response.sprites.front_default
+}
+
+
+response()
+
+btn.addEventListener("click",response)
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}", // Scanne tous les fichiers dans `src`
+    "./main.html"           // Scanne le fichier HTML principal
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
